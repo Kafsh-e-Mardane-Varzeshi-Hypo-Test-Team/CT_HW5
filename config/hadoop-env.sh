85af6690f4acbe6fdfcad41d4d3b4f5db56389bf
mammedbrk <mammedbrk@gmail.com>
2025-08-03 19:23:06 +0330

    feat: add hadoop-env.sh

---
+++ config/hadoop-env.sh
@@ -0,0 +1,7 @@
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-arm64
+# change to amd64 if you are using a amd64 machine
+# export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+export HDFS_NAMENODE_USER=root
+export HDFS_DATANODE_USER=root
+export HDFS_SECONDARYNAMENODE_USER=root
+
